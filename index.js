b7bc3481a3875a9646b6e9517338f3c8c22dcd59
Sabarinath <sabarinath.dev15@gmail.com>
2025-09-17 21:43:40 +0530

    Init Express Js

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const http = require("http");
+const express = require("express");
 
-console.log("Hello World! Welcome to Node.js");
+const app = express();
 
-console.log("Starting of event loop");
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
-setTimeout(() => console.log("This is a set timeout"), 0);
-setImmediate(() => console.log("This is a set immediate"));
-process.nextTick(() => console.log("This is a process next tick"));
-Promise.resolve().then(() => console.log("This is a promise resolve"));
-
-console.log("Ending of event loop");
-
-//Raw node js server
-
-const server = http.createServer((req, res) => {
-    console.log("Request received : ", req.url);
-
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Hello World</h1>");
-})
-
-server.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(3000, () => console.log("Server is running on port 3000"));
