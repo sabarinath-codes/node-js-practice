fa12f88b2eff37b4e6841e328ef56a8f77557b3d
Sabarinath <sabarinath.dev15@gmail.com>
2025-09-17 21:23:46 +0530

    Node js Server

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const http = require("http");
+
 console.log("Hello World! Welcome to Node.js");
 
 console.log("Starting of event loop");
@@ -8,3 +10,14 @@ process.nextTick(() => console.log("This is a process next tick"));
 Promise.resolve().then(() => console.log("This is a promise resolve"));
 
 console.log("Ending of event loop");
+
+//Raw node js server
+
+const server = http.createServer((req, res) => {
+    console.log("Request received : ", req.url);
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Hello World</h1>");
+})
+
+server.listen(3000, () => console.log("Server is running on port 3000"));
